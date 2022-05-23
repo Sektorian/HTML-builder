61ecd8c05ead87b4d1847e655a8ba79ef8728e28
Sektorian <g.sektorian@gmail.com>
2022-05-24 00:06:42 +0300

    feat: add solution to task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathSrc = path.join(__dirname, 'files');
+const pathDest = path.join(__dirname, 'files-copy');
+
+    fs.mkdir(pathDest, {recursive: true}, (err) => {
+        if(err) {console.log('Возникла ошибка:\n', error.message)}
+    });
+
+    fs.readdir(pathDest, (err, data) => {
+        if(err) {console.log('Возникла ошибка:\n', error.message)}
+        
+        data.forEach(file => {
+
+            fs.unlink(path.join(pathDest, file), (err) => {
+                if(err) {console.log('Возникла ошибка:\n', error.message)}
+            });
+        })
+    });
+
+    fs.readdir(pathSrc, (err, data) => {
+        if(err) {console.log('Возникла ошибка:\n', error.message)}
+        
+        data.forEach(file => {
+
+            fs.copyFile(path.join(pathSrc, file), path.join(pathDest, file), (err) => {
+                if(err) {console.log('Возникла ошибка:\n', error.message)}
+            });
+
+        })
+    });
+    
