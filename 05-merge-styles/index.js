fa29e833339f73b2e83e8f5d986de565c7d3b1f7
Sektorian <g.sektorian@gmail.com>
2022-05-24 01:56:58 +0300

    fix: fixed solution to task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,57 +1,29 @@
-//Импорт всех требуемых модулей
 const fs = require('fs');
 const path = require('path');
 
-//Чтение содержимого папки styles
-
 const pathToFile = path.join(__dirname, 'styles');
 const destFile = path.join(__dirname, 'project-dist', 'bundle.css');
-
 const output = fs.createWriteStream(destFile);
-
-
 
 fs.readdir(pathToFile, {withFileTypes: true}, (err, data) => {
     if(err) {console.log('Возникла ошибка:\n', error.message)}
     
-    data.forEach(file => {
-        if (file.isFile()) {
-            const pathToCurFile = path.join(pathToFile, file.name);
-            
+    data.forEach(fileCSS => {
+        if (fileCSS.isFile()) {
+            const pathToCurFile = path.join(pathToFile, fileCSS.name);
+            const input = fs.createReadStream(pathToCurFile, 'utf-8');
+
             fs.stat(pathToCurFile, (err, stats) => {
                 if(err) {console.log('Возникла ошибка:\n', error.message)}
-                const fileExt = path.extname(path.join(pathToFile, file.name));
+                const fileExt = path.extname(path.join(pathToFile, fileCSS.name));
 
                 if(fileExt.slice(1) === 'css') {
-                    const input = fs.createReadStream(pathToCurFile, 'utf-8');
-                    input.on('data', chunk => data += chunk);
-                    input.on('end', () => output.write(data));
+                    let text = '';
+                    input.on('data', chunk => text += chunk);
+                    input.on('end', () => output.write(text));
                     input.on('error', error => console.log('Возникла ошибка:\n', error.message));
                 }
             });
         }
     })
 });
-
-
-
-
-
-
-
-
-/*
-const file = path.join(__dirname, 'text.txt');
-const input = fs.createReadStream(file, 'utf-8');
-let data = '';
-input.on('data', chunk => data += chunk);
-input.on('end', () => console.log(data));
-input.on('error', error => console.log('Возникла ошибка:\n', error.message));
-
-*/
-
-
-//Проверка является ли объект файлом и имеет ли файл нужное расширение
-//Чтение файла стилей
-//Запись прочитанных данных в массив
-//Запись массива стилей в файл bundle.css
